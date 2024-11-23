ae9e3be4880c79635cec09342511980f1d5a0c10
Maryam Shakeel <157610901+maryamshakeel22@users.noreply.github.com>
2024-11-23 09:48:29 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
     <>
         <footer className="bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
           <div className="flex flex-row gap-4">
-          Made by Maryam Shakeel   
-          <Link href={'linkedin.com/in/maryam-shakeel-5720a12b5/'}><FaLinkedin size={24}/></Link>
+          Created by Maryam Shakeel   
+          <Link href={'https://www.linkedin.com/in/maryam-shakeel-5720a12b5/'}><FaLinkedin size={24}/></Link>
           <Link href={'https://portfolio-maryam-shakeels-projects.vercel.app/'}><TbWorld size={24}/></Link>
           </div>
       </footer></>
